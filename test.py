f247f26c059b5626acbbd851a8b5e5d7830f5476
Alex Ray <a@machinaut.com>
2018-01-02 14:40:40 -0800

    cleanup test prints

--- test.py
+++ test.py
@@ -10,32 +10,26 @@ def play(azero):
     ''' Play a game with the human '''
     while True:
         print('\nNew Game!')
-        if np.random.random() < .5:  # Human starts
-            player = 'human'
-        else:
-            player = 'azero'
+        print('Doc:', azero.game.__doc__)
+        human = np.random.random() < .5  # Human starts
         state = azero.game.start()
         while state is not None:
-            print('Turn:', player)
+            print('Turn:', 'human' if human else 'azero')
             print('State:', azero.game.human(state))
-            if player == 'human':
+            if human:
                 print('Valid:', np.flatnonzero(game.valid(state)))
                 action = int(input('Move:'))
-                player = 'azero'
-            elif player == 'azero':
+            else:
                 probs, _ = azero.model.model(state)
                 probs += np.where(azero.game.valid(state), 0, -np.inf)
                 print('Probs:', probs)
                 action = np.argmax(probs)
                 print('Move:', action)
-                player = 'human'
-            else:
-                raise ValueError('invalid player!')
+            human = not human  # Switch players
             state, outcome = game.step(state, action)
-        if outcome < 0:
+        player = 'human' if human ^ (outcome > 0) else 'azero'
+        if outcome != 0:
             print('Outcome:', player, 'wins!')
-        elif outcome > 0:
-            print('Outcome:', player, 'loses!')
         else:
             print('Outcome: draw!')
 
