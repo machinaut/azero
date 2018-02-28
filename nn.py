80a0a7f764ed855b73d80ac36334c97bc1d646a8
Alex Ray <a@machinaut.com>
2018-02-28 10:30:32 -0800

    add loss

--- nn.py
+++ nn.py
@@ -31,3 +31,26 @@ def mlp_bak(dout, cache):
     dW = x.T.dot(dout)
     db = dout.sum(axis=0)
     return dx, dW, db
+
+
+def loss_fwd(x, q, v, z, c):
+    ''' softmax cross-entropy and mean-squared-error combination - forward '''
+    logits = x - np.max(x, axis=1, keepdims=True)
+    e = np.exp(logits)
+    Z = np.sum(e, axis=1, keepdims=True)
+    logp = logits - np.log(Z)
+    xent = np.sum(logp * q, axis=1, keepdims=True)
+    d = v - z
+    mse = np.sum(np.square(d), axis=1, keepdims=True)
+    out = c * xent + (1 - c) * mse
+    cache = (q, e, Z, d, c)
+    return out, cache
+
+
+def loss_bak(dout, cache):
+    ''' softmax cross-entropy and mean-squared-error combination - backward '''
+    q, e, Z, d, c = cache
+    dv = 2 * d * (1 - c) * dout
+    dlogp = c * dout * q
+    dx = dlogp - e * (np.sum(dlogp, axis=1, keepdims=True) / Z)
+    return dx, dv
--- test_nn.py
+++ test_nn.py
@@ -13,6 +13,9 @@ def multi_index_iterator(x):
 
 
 def finite_difference(f, x, df, h=1e-7):
+    assert not np.issubdtype(x.dtype, np.integer)
+    x = x.copy()
+    x.setflags(write=True)
     grad = np.zeros_like(x)
     for ix in multi_index_iterator(x):
         oldval = x[ix]
@@ -52,6 +55,24 @@ class TestNN(unittest.TestCase):
         np.testing.assert_allclose(dx, nx)
         np.testing.assert_allclose(dW, nW)
         np.testing.assert_allclose(db, nb)
+
+    def test_loss(self):
+        rs = np.random.RandomState(0)
+        A, B, C = 5, 4, 3
+        x = rs.randn(A, B)
+        q = rs.randn(A, B)
+        v = rs.randn(A, C)
+        z = rs.randn(A, C)
+        c = rs.randn(1)
+        dout = rs.randn(A, 1)
+        for arr in (x, q, v, z, c, dout):
+            arr.setflags(write=False)
+        out, cache = nn.loss_fwd(x, q, v, z, c)
+        dx, dv = nn.loss_bak(dout, cache)
+        nx = finite_difference(lambda y: nn.loss_fwd(y, q, v, z, c)[0], x, dout)
+        nv = finite_difference(lambda y: nn.loss_fwd(x, q, y, z, c)[0], v, dout)
+        np.testing.assert_allclose(dx, nx, atol=1e-6)
+        np.testing.assert_allclose(dv, nv, atol=1e-6)
 
 
 if __name__ == '__main__':
